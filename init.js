df086906a2339cda57eeb2815078dca1cb8b22e7
SMOLKEYS <jiroshimonis@gmail.com>
2022-03-01 01:09:06 +0800

    no wokr :aaaa:

--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-/const Discord = require("discord.js");
+const Discord = require("discord.js");
 const login = require("./token.json");
 const Filesys = require("fs");
 const Path = require("path");
