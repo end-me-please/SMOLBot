e417963345bb5b588e758598f309daa17d0ced8b
SMOLKEYS <jiroshimonis@gmail.com>
2022-03-01 14:28:42 +0800

    AAAAAAAAAAAAA

--- init.js
+++ init.js
@@ -61,8 +61,7 @@ Bot.on("messageCreate", msg => {
     	break;
 
     	case "av":
-    	let user = msg.mentions.users.first() || msg.content.slice(6);
-    	if(user == "") user = msg.author;
+    	let user = msg.mentions.users.first() || msg.author;
     	const AvEmbed = new Discord.MessageEmbed()
     	.setTitle("Avatar")
     	.setImage(user.displayAvatarURL() + "?size=2048");
