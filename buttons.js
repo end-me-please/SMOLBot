5de6eff64fdf5dd6313435a0cfa8bdda756fdd2d
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-03-09 11:39:32 +0100

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -21,8 +21,23 @@ const EndButton = new Discord.MessageActionRow().addComponents(
 	.setStyle("PRIMARY")
 );
 
+const nhButtonRow = new Discord.MessageActionRow().addComponents(
+	new Discord.MessageButton()
+	.setCustomId("nhPageUp")
+	.setLabel("next")
+	.setStyle("SUCCESS"),
+	new Discord.MessageButton()
+	.setCustomId("nhPageDown")
+	.setLabel("previous")
+	.setStyle("DANGER"),
+	new Discord.MessageButton()
+	.setCustomId("end")
+	.setLabel("close")
+	.setStyle("PRIMARY")
+
 module.exports = {
 	NsfwButton: NsfwButton,
 	HelpButton: HelpButton,
-	EndButton: EndButton
+	EndButton: EndButton,
+	nhButtonRow: nhButtonRow	
 }
